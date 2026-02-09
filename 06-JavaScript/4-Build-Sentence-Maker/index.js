e52dac0a0801bddc47b2eb49ca98643b372d2b77
Alif <ahmadrahman7227@gmail.com>
2026-02-09 14:53:02 +0700

    Practice Creating a Sentence Builder with JavaScript

---
+++ 06-JavaScript/4-Build-Sentence-Maker/index.js
@@ -0,0 +1,27 @@
+// 1. Mendeklarasikan variabel menggunakan 'let' dan menetapkan nilai string pilihan
+let adjective = "brave";
+let noun = "dragon";
+let verb = "excited";
+let place = "crystal cave";
+let adjective2 = "glowing";
+let noun2 = "magical watermelons";
+
+// 2. Mendeklarasikan firstStory dan menggunakan templat cerita pertama
+let firstStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 + ". The " + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".";
+
+// 3. Menampilkan cerita pertama ke konsol
+console.log("First story: " + firstStory);
+
+// 4. Memberikan nilai baru pada variabel-variabel yang ada
+adjective = "clumsy";
+noun = "giant";
+verb = "scared";
+place = "floating island";
+adjective2 = "gigantic";
+noun2 = "golden burgers";
+
+// 5. Mendeklarasikan secondStory dan membuat cerita kedua dengan templat yang sama
+let secondStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 + ". The " + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".";
+
+// 6. Menampilkan cerita kedua ke konsol
+console.log("Second story: " + secondStory);
